a16b2b6e308acfe4bceec98406c7a50ff2bbba8b
TheNorthForty <matt.l.armstrong@gmail.com>
2020-09-06 06:49:05 -0500

    build message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_113844) do
+ActiveRecord::Schema.define(version: 2020_09_06_112555) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
